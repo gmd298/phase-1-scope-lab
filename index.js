be7628f5a719ba967464fa3ec2714d8e249a4a36
Griffin <gmd298@gmail.com>
2022-03-15 10:29:29 -0400

    commit tree 3bb78863e99106521721145b1e2ba5522a4363f6

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function name() {
+    return customerName
+}
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Griffin'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
